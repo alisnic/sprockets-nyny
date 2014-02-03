26edf0b73b479409cf170ed3b78c68cde089035c
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-02-03 20:12:22 +0200

    Example works, yay!

--- lib/sprockets/nyny.rb
+++ lib/sprockets/nyny.rb
@@ -3,7 +3,24 @@ require 'sprockets/rails/helper'
 
 module Sprockets
   module NYNY
-    def registered app
+    def self.load_tasks!
+      #TODO: implement
+    end
+
+    def enable_sprockets options
+      sprockets = Sprockets::Environment.new
+      options.fetch(:paths, []).each {|path| sprockets.append_path(path)}
+      sprockets = sprockets.index if ::NYNY.env.production?
+
+      prefix = options.fetch(:url, '/assets')
+      scope_class.assets_environment  = sprockets
+      scope_class.assets_prefix       = prefix
+      builder.map (prefix) { run sprockets }
+    end
+
+    def self.registered app
+      app.helpers ActionView::Helpers::AssetUrlHelper
+      app.helpers ActionView::Helpers::AssetTagHelper
       app.helpers Sprockets::Rails::Helper
     end
   end
--- test/server.rb
+++ test/server.rb
@@ -1,12 +1,18 @@
 #!ruby -I ../lib -I lib
 require 'nyny'
-require 'sprockets'
 require 'haml'
 require 'coffee_script'
 require 'sprockets/nyny'
 
 class App < NYNY::App
   register Sprockets::NYNY
+
+  enable_sprockets :url => '/assets', :paths => [
+    'vendor/javascripts',
+    'app/assets/javascripts',
+    'app/assets/stylesheets',
+    'app/assets/images'
+  ]
 
   get '/' do
     render 'app/views/index.haml'
